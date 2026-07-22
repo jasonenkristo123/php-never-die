9b532163840ecbbc620fc13e4cb889b6222539e7
jasonenkristo123 <jasonenkristo@gmail.com>
2026-07-22 20:35:20 +0700

    type check

--- index.php
+++ index.php
@@ -9,3 +9,15 @@ $name = "Toj";
 echo $name;
 
 echo "Hai $name";
+
+$nama1 = "Prabowo";
+$nama2 = "Subianto";
+echo "<br>";
+echo $nama1 . $nama2;
+
+define("PI", 3.14);
+echo PI;
+
+var_dump($nama1);
+echo var_dump($nama2);
+echo gettype($nama1);
