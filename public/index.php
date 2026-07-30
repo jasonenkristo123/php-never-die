192437800e77e5a04ff2db03f3e45d9fef8ba974
jasonenkristo123 <jasonenkristo@gmail.com>
2026-07-30 20:48:18 +0700

    refactor: change the logic php

--- public/index.php
+++ public/index.php
@@ -1,12 +1,3 @@
-<?php
-
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $nama = $_POST['Nama'];
-        echo "<h2>Hello, $nama</h2>";
-    }
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,13 +8,49 @@
 <body>
     <h1>
         Student Registration
-    </h1>
+    </h1
 
+    <?php if ($_SERVER['REQUEST_METHOD'] == "POST") :
+    $name = $_POST['name'];
+    $nim = $_POST['nim'];
+    $major = $_POST['major'];
+    $semester = $_POST['semester'];
+
+    if (empty($name) || empty($nim) || empty($major) || empty($semester)) {
+        echo "<p>Please fill in all fields.</p>";
+    } else {
+        echo "<p>Name: " . $name . "</p>";
+        echo "<p>Nim: " . $nim . "</p>";
+        echo "<p>Major: " . $major . "</p>";
+        echo "<p>Semester: " . $semester . "</p>";
+    }
+
+    ?>
+
+    <?php else : ?>
     <form method="POST">
         <div>
-            <label for="Nama">Nama : </label>
+            <label for="name">Name:</label>
             <br>
-            <input type="text" name="Nama">
+            <input type="text" id="name" name="name" required>
+        </div>
+
+        <div>
+            <label for="nim">Nim:</label>
+            <br>
+            <input type="number" id="nim" name="nim">
+        </div>
+
+        <div>
+            <label for="major">Jurusan:</label>
+            <br>
+            <input type="text" id="major" name="major">
+        </div>
+
+        <div>
+            <label for="semester">Semester:</label>
+            <br>
+            <input type="text" id="semester" name="semester">
         </div>
 
         <button type="submit">
@@ -31,5 +58,8 @@
         </button>
     </form>
 
-</body>
-</html>
+    <?php endif; ?>
+
+
+    </body>
+    </html>
