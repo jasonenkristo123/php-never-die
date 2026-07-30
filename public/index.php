a650cc118ddd2f7dadffe0f1c45371aadd2da5e8
jasonenkristo123 <jasonenkristo@gmail.com>
2026-07-30 19:56:04 +0700

    feat: add form

--- public/index.php
+++ public/index.php
@@ -1,3 +1,46 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Student Registration</title>
+</head>
+<body>
+    <h1>
+        Student Registration
+    </h1>
 
-echo  "test";
+    <form method="POST">
+        <div>
+            <label for="name">Name : </label>
+            <input type="text" id="name" name="name">
+        </div>
+
+        <br>
+
+        <div>
+            <label for="nim">Nim : </label>
+            <input type="text" id="nim" name="nim">
+        </div>
+
+        <br>
+
+        <div>
+            <label for="major">Major : </label>
+            <input type="text" id="major" name="major">
+        </div>
+
+        <br>
+
+        <div>
+            <label for="semester">Semester : </label>
+            <input type="text" id="semester" name="semester">
+        </div>
+
+        <button type="submit">
+            Submit
+        </button>
+    </form>
+
+</body>
+</html>
